a80ab7bb52127da2b7ab6e8576456101829a740a
Riceyo <richmond@ricey.org>
2017-06-23 10:11:08 -0500

    test

--- hello.java
+++ hello.java
@@ -2,9 +2,11 @@ public class hello
 	{
 	public static void main(String args[])
 		{
-		for(int i=1; i<5; i++)
+		for(int i=1; i<10; i++)
         		{
 			System.out.println("hello " + i);
         		}
 		}
 	}
+
+
