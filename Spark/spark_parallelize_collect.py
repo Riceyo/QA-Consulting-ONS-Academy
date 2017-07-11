0f45a7d544caf7ab07eb282758744c600f594942
Richmond Rice <richmond@ricey.org>
2017-07-11 14:44:26 +0100

    Update spark_parallelize_collect.py

--- Spark/spark_parallelize_collect.py
+++ Spark/spark_parallelize_collect.py
@@ -6,4 +6,4 @@ list = [1, 2, 3, 4, 5] # create some data
 rdd = sc.parallelize(list) # create the rdd (sends the data out to the nodes in the cluster)
 data = rdd.collect() # read the data from rdd (action method)
 for x in data: # loop the data
-	print(x) # print the data
+	print x # print the data
