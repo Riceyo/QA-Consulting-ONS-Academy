9fffe6a915796b87d99af077bcf0de4545913c8a
Richmond Rice <richmond@ricey.org>
2017-07-11 16:56:14 +0100

    Create spark_calc_results.py

---
+++ Spark/spark_calc_results.py
@@ -0,0 +1,21 @@
+from pyspark import SparkConf, SparkContext
+
+def GetMarks(rec):
+	record = rec.split(",")
+	return record[0], record[2]
+
+def SumMarks(a, b):
+	return int(a) + int(b)
+
+def PerMarks(x):
+	return x * 100 / 300
+
+conf = SparkConf()
+sc = SparkContext(conf = conf)
+rdd = sc.textFile("file:///home/cloudera/Results.txt")
+rddencode = rdd.map(lambda x: x.encode("ascii", "ignore"))
+rddmarks = rddencode.map(GetMarks)
+rddsummarks = rddmarks.reduceByKey(SumMarks)
+rddpermarks = rddsummarks.mapValues(PerMarks)
+data = rddpermarks.collect()
+print data
