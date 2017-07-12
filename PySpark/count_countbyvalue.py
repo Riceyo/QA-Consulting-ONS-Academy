521d46ef24922a9c376de57c558051c08d2ddc5c
Richmond Rice <richmond@ricey.org>
2017-07-12 20:54:01 +0100

    Update count_countbyvalue.py

--- PySpark/count_countbyvalue.py
+++ PySpark/count_countbyvalue.py
@@ -4,8 +4,8 @@ conf = SparkConf()
 sc = SparkContext(conf = conf)
 list = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
 rdd = sc.parallelize(list)
-datacount = rdd.count()
-datacountbyval =  rdd.countByValue()
+datacount = rdd.count() # count values in rdd
+datacountbyval =  rdd.countByValue() # count occurrences of values in rdd
 print(datacount)
 for x in datacountbyval:
 	print x, "-", datacountbyval[x]
