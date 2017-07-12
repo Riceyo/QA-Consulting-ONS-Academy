3621d4e63e7efaf610442793bcc9ff808d382216
Richmond Rice <richmond@ricey.org>
2017-07-12 22:30:05 +0100

    Update saveastextfile.py

--- PySpark/saveastextfile.py
+++ PySpark/saveastextfile.py
@@ -2,6 +2,6 @@ from pyspark import SparkConf, SparkContext
 
 conf = SparkConf()
 sc = SparkContext(conf = conf)
-list = [1,2,3,4,5,6,7,8,9,10,11,12]
-rdd = sc.parallelize(list, 3) # partition the list data into 3 data sets
-rdd.saveAsTextFile("file:///home/cloudera/test/")
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] # create some list data
+rdd = sc.parallelize(list, 3) # partition the list data into 3 slices
+rdd.saveAsTextFile("file:///home/cloudera/test/") # output the rdd to text files
