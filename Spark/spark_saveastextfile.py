bfed87cce3b951cc9dbb5a307430fd640d07143b
Richmond Rice <richmond@ricey.org>
2017-07-12 10:01:18 +0100

    Update spark_saveastextfile.py

--- Spark/spark_saveastextfile.py
+++ Spark/spark_saveastextfile.py
@@ -3,5 +3,5 @@ from pyspark import SparkConf, SparkContext
 conf = SparkConf()
 sc = SparkContext(conf = conf)
 list = [1,2,3,4,5,6,7,8,9,10,11,12]
-rdd = sc.parallelize(list, 3) # partition 
+rdd = sc.parallelize(list, 3) # partition the list data into 3 datasets
 rdd.saveAsTextFile("file:///home/cloudera/test/")
