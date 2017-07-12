0a7c135c77c8212d3c9b7f0b03b305f302d9802d
Richmond Rice <richmond@ricey.org>
2017-07-12 21:32:01 +0100

    Update mapvalues.py

--- PySpark/mapvalues.py
+++ PySpark/mapvalues.py
@@ -1,15 +1,15 @@
 from pyspark import SparkConf, SparkContext
 
-def SalaryBonus(rec):
-	if rec < 20000:
-		return rec + 100
+def SalaryBonus(x):
+	if x < 20000:
+		return x + 100
 	else:
-		return rec + 200
+		return x + 200
 
 conf = SparkConf()
 sc = SparkContext(conf = conf)
-salaries = [['rich', 19000], ['luke', 21000], ['dave', 22000]]
+salaries = [['Richmond', 19000], ['Luke', 21000], ['David', 22000]]
 rdd = sc.parallelize(salaries)
-rddnewsal = rdd.mapValues(SalaryBonus)
+rddnewsal = rdd.mapValues(SalaryBonus) # apply function to every value in the rdd
 data = rddnewsal.collect()
 print data
