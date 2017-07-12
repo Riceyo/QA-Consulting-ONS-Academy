35e19c69052ea59af1c26bf9e6d94ec8ca853158
Richmond Rice <richmond@ricey.org>
2017-07-12 22:51:14 +0100

    Update textfile.py

--- PySpark/textfile.py
+++ PySpark/textfile.py
@@ -1,22 +1,22 @@
 from pyspark import SparkConf, SparkContext
 
-def GetMale(rec):
-	record = rec.split("|")
-	if record[2] == "M":
+def GetMale(x):
+	split = x.split("|") # split the rows data by pipe
+	if split[2] == "M":
 		return True
 	else:
 		return False
 
-def GetFemale(rec):
-	record = rec.split("|")
-	if record[2] == "F":
+def GetFemale(x):
+	split = x.split("|") # split the rows data by pipe
+	if split[2] == "F":
 		return True
 	else:
 		return False
 
 conf = SparkConf()
 sc = SparkContext(conf = conf)
-rdd = sc.textFile("file:///home/cloudera/Users.txt") # read a text file
+rdd = sc.textFile("file:///home/cloudera/Users.txt") # read text file into an rdd row by row, but does not split the rows values
 rddmale = rdd.filter(GetMale)
 rddfemale = rdd.filter(GetFemale)
 malecount = rddmale.count()
