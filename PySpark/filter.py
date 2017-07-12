fe30516302e443559265dc7389cab112a94b40da
Richmond Rice <richmond@ricey.org>
2017-07-12 21:27:52 +0100

    Update filter.py

--- PySpark/filter.py
+++ PySpark/filter.py
@@ -2,14 +2,14 @@ from pyspark import SparkConf, SparkContext
 
 def overtenchecker(x):
 	if x > 10:
-		return True
+		return True # true keeps element in new rdd
 	else:
-		return False
+		return False # false removes element from new rdd
 
 conf = SparkConf()
 sc = SparkContext(conf = conf)
 list = [1, 2, 3, 11, 12, 13]
 rdd = sc.parallelize(list)
-rddfilt = rdd.filter(overtenchecker) # filter rdd elements into new rdd
+rddfilt = rdd.filter(overtenchecker) # run function on the rdd to filter elements into new rdd
 data = rddfilt.collect()
 print data
