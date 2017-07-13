7d9d3172800316a619ffb7485a218e56099fc6c1
Richmond Rice <richmond@ricey.org>
2017-07-13 19:36:01 +0100

    Update task_movies_goldeneye_ratings.py

--- PySpark/task_movies_goldeneye_ratings.py
+++ PySpark/task_movies_goldeneye_ratings.py
@@ -2,10 +2,10 @@ from pyspark import SparkConf, SparkContext
 
 conf = SparkConf()
 sc = SparkContext(conf = conf)
-rddmovies = sc.textFile("file:///home/cloudera/movie_data/Movies.item", 0)
-rddratings = sc.textFile("file:///home/cloudera/movie_data/Moving-Ratings-Done.data", 0)
-datamovies = rddmovies.map(lambda x: x.encode("ascii", "ignore").split("|")).collect()
-dataratings = rddratings.map(lambda x: x.encode("ascii", "ignore").split("\t")).collect()
+rddmovies = sc.textFile("file:///home/cloudera/movie_data/Movies.item", 0) # create movies rdd
+rddratings = sc.textFile("file:///home/cloudera/movie_data/Moving-Ratings-Done.data", 0) # create ratings rdd
+datamovies = rddmovies.map(lambda x: x.encode("ascii", "ignore").split("|")).collect() # encode, split, collect
+dataratings = rddratings.map(lambda x: x.encode("ascii", "ignore").split("\t")).collect() #encode, split, collect
 
 ratingcount5 = 0
 ratingcount4 = 0
@@ -13,19 +13,19 @@ ratingcount3 = 0
 ratingcount2 = 0
 ratingcount1 = 0
 
-for moviesloop in datamovies:
-	for ratingsloop in dataratings:
-		if moviesloop[0] == ratingsloop[1]:
-			if moviesloop[1] == "GoldenEye (1995)" and ratingsloop[2] == "5":
-				ratingcount5 = ratingcount5 + 1
-			if moviesloop[1] == "GoldenEye (1995)" and ratingsloop[2] == "4":
-				ratingcount4 = ratingcount4 + 1
-			if moviesloop[1] == "GoldenEye (1995)" and ratingsloop[2] == "3":
-				ratingcount3 = ratingcount3 + 1
-			if moviesloop[1] == "GoldenEye (1995)" and ratingsloop[2] == "2":
-				ratingcount2 = ratingcount2 + 1
-			if moviesloop[1] == "GoldenEye (1995)" and ratingsloop[2] == "1":
-				ratingcount1 = ratingcount1 + 1
+for moviesloop in datamovies: # loop the movies
+	for ratingsloop in dataratings: # loop the ratings
+		if moviesloop[0] == ratingsloop[1]: # if the movie id matches in both data sets
+			if moviesloop[1] == "GoldenEye (1995)" and ratingsloop[2] == "5": # if goldeneye and 5 rating
+				ratingcount5 = ratingcount5 + 1 # count 5 ratings
+			if moviesloop[1] == "GoldenEye (1995)" and ratingsloop[2] == "4": # if goldeneye and 4 rating
+				ratingcount4 = ratingcount4 + 1 # count 4 ratings
+			if moviesloop[1] == "GoldenEye (1995)" and ratingsloop[2] == "3": # if goldeneye and 3 rating
+				ratingcount3 = ratingcount3 + 1 # count 3 ratings
+			if moviesloop[1] == "GoldenEye (1995)" and ratingsloop[2] == "2": # if goldeneye and 2 rating
+				ratingcount2 = ratingcount2 + 1 # count 2 ratings
+			if moviesloop[1] == "GoldenEye (1995)" and ratingsloop[2] == "1": # if goldeneye and 1 rating
+				ratingcount1 = ratingcount1 + 1 # count 1 ratings
 
 print "5 -", ratingcount5
 print "4 -", ratingcount4
