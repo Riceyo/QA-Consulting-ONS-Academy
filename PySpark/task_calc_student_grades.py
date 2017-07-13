f3f60b9c841735d39f4157c7d4d9e27eb460d901
Richmond Rice <richmond@ricey.org>
2017-07-13 11:17:22 +0100

    Update task_calc_student_grades.py

--- PySpark/task_calc_student_grades.py
+++ PySpark/task_calc_student_grades.py
@@ -25,30 +25,34 @@ def CalcGrade(x):
 
 conf = SparkConf()
 sc = SparkContext(conf = conf)
-rddper = sc.textFile("file:///home/cloudera/student_perinfo.csv")
-rddres = sc.textFile("file:///home/cloudera/student_results.csv")
+rddper = sc.textFile("file:///home/cloudera/student_perinfo.csv", 0)
+rddres = sc.textFile("file:///home/cloudera/student_results.csv", 0)
 rddperhead = rddper.first() # get the header (first row) from the student personal info data
 rddreshead = rddres.first() # get the header (first row) from the student results data
-rddperremhead = rddper.filter(lambda line: line != rddperhead) # remove header from the data
-rddresremhead = rddres.filter(lambda line: line != rddreshead) # remove header from the data
+rddperremhead = rddper.filter(lambda line: line != rddperhead).map(lambda x: x.encode("ascii", "ignore")) # remove header from the data and encode
+rddresremhead = rddres.filter(lambda line: line != rddreshead).map(lambda x: x.encode("ascii", "ignore")) # remove header from the data and encode
 rddpersplit = rddperremhead.map(SplitPer) # split the data
 rddressplit = rddresremhead.map(SplitRes) # split the data
 rddresadd = rddressplit.reduceByKey(Add) # sum the students marks
 dataper = rddpersplit.collect()
 datares = rddressplit.collect()
 dataresadd = rddresadd.collect()
-print dataper
-print datares
-print dataresadd
+datarescount = rddressplit.countByKey() # count how marks we have for the student
+bestpercent = 0
 for resloop in dataresadd: # loop the student results summed up data
 	for perloop in dataper: # loop the student personal info data
 		if perloop[0] == resloop[0]: # if the regno match
-			count = len(perloop[0]) # count how many marks we have for the student - CHECK THIS
+			count = datarescount[perloop[0]] # count how many marks we have for the student
 			percent = resloop[1] * 100 / int(str(count) + '00') # calculate their percentage based on how many marks we have for them
 			grade = CalcGrade(percent) # calculate their grade based on their percent
+			if percent > bestpercent:
+				bestpercent = percent
+				beststudent = perloop[1]
 			print ""
 			print "student reg number:", perloop[0]
 			print "student name:", perloop[1]
 			print "exams taken:", count
 			print "percentage:", percent, "%"
 			print "grade:", grade
+print ""
+print "best student:", beststudent
