38855ee4c6769ae9e89fef6644636a6ba9d67807
Richmond Rice <richmond@ricey.org>
2017-07-19 11:03:09 +0100

    Update polymorphism.scala

--- Scala/polymorphism.scala
+++ Scala/polymorphism.scala
@@ -2,17 +2,21 @@ class draw
 	{
 	def drawing()
 		{
-		println("drawing")
+		println("draw - drawing")
 		}
 	}
 
 class line extends draw
 	{
-	override def drawing()
+	override def drawing() // this method must be overwritten to be used
 		{
-		println("draw line")
+		println("line - drawing")
+		}
+	def writing()
+		{
+		println("line - writing") // this method cannot be used as it's not inherited
 		}
 	}
 	
-var x:draw = new line()
-x.drawing
+var x:draw = new line() // create reference variable of parent class and store address of child class
+x.drawing // call overwritten inherited method
