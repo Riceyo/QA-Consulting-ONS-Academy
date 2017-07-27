0220a0fe7303b21e3d42543684b713f57f1e4231
Richmond Rice <richmond@ricey.org>
2017-07-27 19:15:56 +0100

    Update task_rximport_cars.R

--- R/task_rximport_cars.R
+++ R/task_rximport_cars.R
@@ -13,5 +13,5 @@ dfcars["pricebhpratio"] <- dfcars$MSRP / dfcars$Horsepower # add column to data 
 print(paste("cars asia rear drive count:", carsasiareardrive))
 print(paste("cars euro avg bhp:", carseuroavgbhp))
 print(paste("cars usa avg price:", carsusaavgprice))
-print("cars avg price / bhp ratio  by region:")
+print("cars avg price / bhp ratio by region:")
 print(aggregate(x=dfcars$pricebhpratio, by=list(Region = dfcars$Origin), FUN = mean)) # average price / bhp ratio by region
